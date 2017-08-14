1492fa1056f48aa07dcd284112ff0db8dbd09e67
qianzhaicun <qianzhaicun@163.com>
2017-08-14 16:24:50 +0800

    a scrape zipru_scraper

---
+++ zipru_scraper/zipru_scraper/spiders/zipru_spider.py
@@ -0,0 +1,25 @@
+import scrapy
+
+class ZipruSpider(scrapy.Spider):
+	name = 'zipru'
+	start_urls = ['http://zipru.to/torrents.php?category=TV']    
+	def parse(self, response):
+        # proceed to other pages of the listings
+		for page_url in response.css('a[title ~= page]::attr(href)').extract():
+			page_url = response.urljoin(page_url)
+			yield scrapy.Request(url=page_url, callback=self.parse)
+
+		for tr in response.css('table.lista2t tr.lista2'):
+			tds = tr.css('td')
+			link = tds[1].css('a')[0]
+			yield {
+				'title':link.css('::attr(title)').extract_first(),
+				'url':response.urljoin(link.css('""attr(href)').extarct_first()),
+				'date':tds[2].css('::text').extract_first(),
+				'size':tds[3].css('::text').extract_first(),
+				'seeders':int(tds[4].css('::text').extract_first()),
+				'leechers':int(tds[5].css('::text').extract_first()),
+				'uploader':tds[7].css('::text').extract_first(),
+			}
+
+
